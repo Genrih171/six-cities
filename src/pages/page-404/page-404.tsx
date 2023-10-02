1cd40c05db9439a29bb367add39850df70bd95e8
Genrih <genrih171@rambler.ru>
2023-10-02 19:57:46 +0400

    Выделяет стили в css модуль

--- src/pages/page-404/page-404.tsx
+++ src/pages/page-404/page-404.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styles from './page-404.module.css';
+
 function Page404(): React.JSX.Element {
   return (
     <React.Fragment>
-      <div style={{
-        display: 'inline-block',
-        width: '100%',
-        paddingTop: '50px',
-        paddingBottom: '50px',
-        backgroundColor: '#a1a1a1',
-        textAlign: 'center',
-        fontSize: '30px'
-      }}
-      >404. Page not found.
+      <div className={styles.title}>404. Page not found.
       </div>
-      <div style={{
-        display: 'inline-block',
-        width: '100%',
-        paddingTop: '50px',
-        textAlign: 'center',
-        textDecoration: 'underline'
-      }}
-      ><Link to={'/'}>Return to main</Link>
+      <div className={styles.link}><Link to={'/'}>Return to main</Link>
       </div>
     </React.Fragment>
   );
