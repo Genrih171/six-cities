cd99b0343be87344e5b836d5fa79c0761c0af05e
Genrih <genrih171@rambler.ru>
2023-10-19 17:26:53 +0400

    Реализует показ меню сортировки

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import Header from '../../components/header/header';
@@ -12,6 +13,23 @@ type MainScreenProps = {
 };
 
 function MainPage({offers}: MainScreenProps): React.JSX.Element {
+  const placesOption = useRef<HTMLUListElement | null>(null);
+  const placesSort = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    const openPlacesOption = () => {
+      if (placesOption.current) {
+        placesOption.current.classList.toggle('places__options--opened');
+      }
+    };
+
+    const placesSortButton = placesSort.current;
+
+    placesSortButton?.addEventListener('click', openPlacesOption);
+
+    return () => placesSortButton?.removeEventListener('click', openPlacesOption);
+  }, []);
+
   return (
     <div className="page page--gray page--main">
       <Helmet>
@@ -33,14 +51,14 @@ function MainPage({offers}: MainScreenProps): React.JSX.Element {
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">312 places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
+                <span className="places__sorting-caption">Sort by </span>
+                <span ref={placesSort} className="places__sorting-type" tabIndex={0}>
                   Popular
                   <svg className="places__sorting-arrow" width="7" height="4">
                     <use xlinkHref="#icon-arrow-select"></use>
                   </svg>
                 </span>
-                <ul className="places__options places__options--custom places__options--opened">
+                <ul ref={placesOption} className="places__options places__options--custom">
                   <li className="places__option places__option--active" tabIndex={0}>Popular</li>
                   <li className="places__option" tabIndex={0}>Price: low to high</li>
                   <li className="places__option" tabIndex={0}>Price: high to low</li>
